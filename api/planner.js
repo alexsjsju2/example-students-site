8851a001ca32f5de68747e1c9afd15ec3e257296
alexsjsju2 <atm194@proton.me>
2026-07-31 16:49:28 +0200

    Update planner.js

--- api/planner.js
+++ api/planner.js
@@ -16,11 +16,22 @@ const db = admin.firestore();
 const hash = str => crypto.createHash('sha256').update(String(str)).digest('hex');
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.alexsjsju.eu');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    const allowedOrigins = ['https://www.alexsjsju.eu', 'https://alexsjsju.eu'];
+    const origin = req.headers.origin;
 
-    if (req.method === 'OPTIONS') return res.status(200).end();
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', 'https://www.alexsjsju.eu');
+    }
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
 
     try {
         if (req.method === 'POST') {
